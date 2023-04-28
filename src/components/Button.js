c90f002f24d049363a926f0b8eaed65150dc6711
Arash <arashn2y@gmail.com>
2023-04-28 21:42:35 +0200

    button update

--- src/components/Button.js
+++ src/components/Button.js
@@ -7,7 +7,7 @@ const Button = (props) => {
     
     return(
         <button
-            className={`border-none rounded-md px-2 py-2 cursor-pointer font-normal transition-all ease-in duration-100 disabled:cursor-default disabled:bg-slate-100 disabled:text-slate-500 hover:enabled:${hover} hover:enabled:text-white ${bgColor} ${marginRight}`}
+            className={`border-none rounded-md px-2 py-2 cursor-pointer font-normal transition-all ease-in duration-100 disabled:cursor-default disabled:bg-slate-100 disabled:text-slate-500 hover:enabled:${hover} ${bgColor} ${marginRight}`}
             onClick={onClick}
             disabled={disabled}>
             {text}
